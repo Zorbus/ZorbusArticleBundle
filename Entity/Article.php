1290f0a355a6ec1f984124aa8edc4dc023cba23b
Tito Miguel Costa <github@titomiguelcosta.com>
2012-11-05 13:20:54 +0000

    added superclass to article entity

--- Entity/Article.php
+++ Entity/Article.php
@@ -9,7 +9,68 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Article extends Base\Article
 {
+    /**
+     * @ORM\PrePersist
+     */
+    public function preImageUpload()
+    {
+        if (null !== $this->imageTemp)
+        {
+            $this->image = sha1(uniqid(mt_rand(), true)) . '.' . $this->imageTemp->guessExtension();
+        }
+    }
 
+    /**
+     * @ORM\PrePersist
+     */
+    public function preAttachmentUpload()
+    {
+        if (null !== $this->attachmentTemp)
+        {
+            $this->attachment = sha1(uniqid(mt_rand(), true)) . '.' . $this->attachmentTemp->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostRemove
+     */
+    public function postRemove()
+    {
+        if ($file = $this->getAbsolutePath($this->image))
+        {
+            @unlink($file);
+        }
+        if ($file = $this->getAbsolutePath($this->attachment))
+        {
+            @unlink($file);
+        }
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function postImageUpload()
+    {
+        if ($this->imageTemp instanceof \Symfony\Component\HttpFoundation\File\UploadedFile)
+        {
+            $this->imageTemp->move($this->getUploadRootDir(), $this->image);
+
+            unset($this->imageTemp);
+        }
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function postAttachmentUpload()
+    {
+        if ($this->attachmentTemp instanceof \Symfony\Component\HttpFoundation\File\UploadedFile)
+        {
+            $this->attachmentTemp->move($this->getUploadRootDir(), $this->attachment);
+
+            unset($this->attachmentTemp);
+        }
+    }
     /**
      * @var integer $id
      */
@@ -24,6 +85,11 @@ class Article extends Base\Article
      * @var string $subtitle
      */
     private $subtitle;
+
+    /**
+     * @var string $resume
+     */
+    private $resume;
 
     /**
      * @var string $body
@@ -135,12 +201,6 @@ class Article extends Base\Article
      */
     private $tags;
 
-
-    /**
-     * @var string $resume
-     */
-    private $resume;
-
     /**
      * Constructor
      */
@@ -149,11 +209,11 @@ class Article extends Base\Article
         $this->categories = new \Doctrine\Common\Collections\ArrayCollection();
         $this->tags = new \Doctrine\Common\Collections\ArrayCollection();
     }
-
+    
     /**
      * Get id
      *
-     * @return integer
+     * @return integer 
      */
     public function getId()
     {
@@ -169,14 +229,14 @@ class Article extends Base\Article
     public function setTitle($title)
     {
         $this->title = $title;
-
+    
         return $this;
     }
 
     /**
      * Get title
      *
-     * @return string
+     * @return string 
      */
     public function getTitle()
     {
@@ -192,18 +252,41 @@ class Article extends Base\Article
     public function setSubtitle($subtitle)
     {
         $this->subtitle = $subtitle;
-
+    
         return $this;
     }
 
     /**
      * Get subtitle
      *
-     * @return string
+     * @return string 
      */
     public function getSubtitle()
     {
         return $this->subtitle;
+    }
+
+    /**
+     * Set resume
+     *
+     * @param string $resume
+     * @return Article
+     */
+    public function setResume($resume)
+    {
+        $this->resume = $resume;
+    
+        return $this;
+    }
+
+    /**
+     * Get resume
+     *
+     * @return string 
+     */
+    public function getResume()
+    {
+        return $this->resume;
     }
 
     /**
@@ -215,14 +298,14 @@ class Article extends Base\Article
     public function setBody($body)
     {
         $this->body = $body;
-
+    
         return $this;
     }
 
     /**
      * Get body
      *
-     * @return string
+     * @return string 
      */
     public function getBody()
     {
@@ -238,14 +321,14 @@ class Article extends Base\Article
     public function setLang($lang)
     {
         $this->lang = $lang;
-
+    
         return $this;
     }
 
     /**
      * Get lang
      *
-     * @return string
+     * @return string 
      */
     public function getLang()
     {
@@ -261,14 +344,14 @@ class Article extends Base\Article
     public function setType($type)
     {
         $this->type = $type;
-
+    
         return $this;
     }
 
     /**
      * Get type
      *
-     * @return string
+     * @return string 
      */
     public function getType()
     {
@@ -284,14 +367,14 @@ class Article extends Base\Article
     public function setAttachment($attachment)
     {
         $this->attachment = $attachment;
-
+    
         return $this;
     }
 
     /**
      * Get attachment
      *
-     * @return string
+     * @return string 
      */
     public function getAttachment()
     {
@@ -307,14 +390,14 @@ class Article extends Base\Article
     public function setImage($image)
     {
         $this->image = $image;
-
+    
         return $this;
     }
 
     /**
      * Get image
      *
-     * @return string
+     * @return string 
      */
     public function getImage()
     {
@@ -330,14 +413,14 @@ class Article extends Base\Article
     public function setStatus($status)
     {
         $this->status = $status;
-
+    
         return $this;
     }
 
     /**
      * Get status
      *
-     * @return string
+     * @return string 
      */
     public function getStatus()
     {
@@ -353,14 +436,14 @@ class Article extends Base\Article
     public function setAuthor($author)
     {
         $this->author = $author;
-
+    
         return $this;
     }
 
     /**
      * Get author
      *
-     * @return string
+     * @return string 
      */
     public function getAuthor()
     {
@@ -376,14 +459,14 @@ class Article extends Base\Article
     public function setSource($source)
     {
         $this->source = $source;
-
+    
         return $this;
     }
 
     /**
      * Get source
      *
-     * @return string
+     * @return string 
      */
     public function getSource()
     {
@@ -399,14 +482,14 @@ class Article extends Base\Article
     public function setLocal($local)
     {
         $this->local = $local;
-
+    
         return $this;
     }
 
     /**
      * Get local
      *
-     * @return string
+     * @return string 
      */
     public function getLocal()
     {
@@ -422,14 +505,14 @@ class Article extends Base\Article
     public function setIsHighlighted($isHighlighted)
     {
         $this->is_highlighted = $isHighlighted;
-
+    
         return $this;
     }
 
     /**
      * Get is_highlighted
      *
-     * @return boolean
+     * @return boolean 
      */
     public function getIsHighlighted()
     {
@@ -445,14 +528,14 @@ class Article extends Base\Article
     public function setEnabled($enabled)
     {
         $this->enabled = $enabled;
-
+    
         return $this;
     }
 
     /**
      * Get enabled
      *
-     * @return boolean
+     * @return boolean 
      */
     public function getEnabled()
     {
@@ -468,14 +551,14 @@ class Article extends Base\Article
     public function setDateShow($dateShow)
     {
         $this->date_show = $dateShow;
-
+    
         return $this;
     }
 
     /**
      * Get date_show
      *
-     * @return \DateTime
+     * @return \DateTime 
      */
     public function getDateShow()
     {
@@ -491,14 +574,14 @@ class Article extends Base\Article
     public function setDateHide($dateHide)
     {
         $this->date_hide = $dateHide;
-
+    
         return $this;
     }
 
     /**
      * Get date_hide
      *
-     * @return \DateTime
+     * @return \DateTime 
      */
     public function getDateHide()
     {
@@ -514,14 +597,14 @@ class Article extends Base\Article
     public function setDatePublished($datePublished)
     {
         $this->date_published = $datePublished;
-
+    
         return $this;
     }
 
     /**
      * Get date_published
      *
-     * @return \DateTime
+     * @return \DateTime 
      */
     public function getDatePublished()
     {
@@ -537,14 +620,14 @@ class Article extends Base\Article
     public function setDateEvent($dateEvent)
     {
         $this->date_event = $dateEvent;
-
+    
         return $this;
     }
 
     /**
      * Get date_event
      *
-     * @return string
+     * @return string 
      */
     public function getDateEvent()
     {
@@ -560,14 +643,14 @@ class Article extends Base\Article
     public function setUserId($userId)
     {
         $this->user_id = $userId;
-
+    
         return $this;
     }
 
     /**
      * Get user_id
      *
-     * @return integer
+     * @return integer 
      */
     public function getUserId()
     {
@@ -583,14 +666,14 @@ class Article extends Base\Article
     public function setObservations($observations)
     {
         $this->observations = $observations;
-
+    
         return $this;
     }
 
     /**
      * Get observations
      *
-     * @return string
+     * @return string 
      */
     public function getObservations()
     {
@@ -606,14 +689,14 @@ class Article extends Base\Article
     public function setSlug($slug)
     {
         $this->slug = $slug;
-
+    
         return $this;
     }
 
     /**
      * Get slug
      *
-     * @return string
+     * @return string 
      */
     public function getSlug()
     {
@@ -629,14 +712,14 @@ class Article extends Base\Article
     public function setCreatedAt($createdAt)
     {
         $this->created_at = $createdAt;
-
+    
         return $this;
     }
 
     /**
      * Get created_at
      *
-     * @return \DateTime
+     * @return \DateTime 
      */
     public function getCreatedAt()
     {
@@ -652,14 +735,14 @@ class Article extends Base\Article
     public function setUpdatedAt($updatedAt)
     {
         $this->updated_at = $updatedAt;
-
+    
         return $this;
     }
 
     /**
      * Get updated_at
      *
-     * @return \DateTime
+     * @return \DateTime 
      */
     public function getUpdatedAt()
     {
@@ -675,7 +758,7 @@ class Article extends Base\Article
     public function addCategorie(\Zorbus\ArticleBundle\Entity\Category $categories)
     {
         $this->categories[] = $categories;
-
+    
         return $this;
     }
 
@@ -692,7 +775,7 @@ class Article extends Base\Article
     /**
      * Get categories
      *
-     * @return Doctrine\Common\Collections\Collection
+     * @return Doctrine\Common\Collections\Collection 
      */
     public function getCategories()
     {
@@ -708,7 +791,7 @@ class Article extends Base\Article
     public function addTag(\Zorbus\ArticleBundle\Entity\Tag $tags)
     {
         $this->tags[] = $tags;
-
+    
         return $this;
     }
 
@@ -725,96 +808,10 @@ class Article extends Base\Article
     /**
      * Get tags
      *
-     * @return Doctrine\Common\Collections\Collection
+     * @return Doctrine\Common\Collections\Collection 
      */
     public function getTags()
     {
         return $this->tags;
-    }
-
-    /**
-     * Set resume
-     *
-     * @param string $resume
-     * @return Article
-     */
-    public function setResume($resume)
-    {
-        $this->resume = $resume;
-
-        return $this;
-    }
-
-    /**
-     * Get resume
-     *
-     * @return string
-     */
-    public function getResume()
-    {
-        return $this->resume;
-    }
-
-    /**
-     * @ORM\PrePersist
-     */
-    public function preImageUpload()
-    {
-        if (null !== $this->imageTemp)
-        {
-            $this->image = sha1(uniqid(mt_rand(), true)) . '.' . $this->imageTemp->guessExtension();
-        }
-    }
-
-    /**
-     * @ORM\PrePersist
-     */
-    public function preAttachmentUpload()
-    {
-        if (null !== $this->attachmentTemp)
-        {
-            $this->attachment = sha1(uniqid(mt_rand(), true)) . '.' . $this->attachmentTemp->guessExtension();
-        }
-    }
-
-    /**
-     * @ORM\PostRemove
-     */
-    public function postRemove()
-    {
-        if ($file = $this->getAbsolutePath($this->image))
-        {
-            @unlink($file);
-        }
-        if ($file = $this->getAbsolutePath($this->attachment))
-        {
-            @unlink($file);
-        }
-    }
-
-    /**
-     * @ORM\PostPersist
-     */
-    public function postImageUpload()
-    {
-        if ($this->imageTemp instanceof \Symfony\Component\HttpFoundation\File\UploadedFile)
-        {
-            $this->imageTemp->move($this->getUploadRootDir(), $this->image);
-
-            unset($this->imageTemp);
-        }
-    }
-
-    /**
-     * @ORM\PostPersist
-     */
-    public function postAttachmentUpload()
-    {
-        if ($this->attachmentTemp instanceof \Symfony\Component\HttpFoundation\File\UploadedFile)
-        {
-            $this->attachmentTemp->move($this->getUploadRootDir(), $this->attachment);
-
-            unset($this->attachmentTemp);
-        }
     }
 }
